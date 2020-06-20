b1a53a92cba599cd2f01bbdb24edfb9d075d0fb5
themaleem <zizo9585@gmail.com>
2020-06-20 11:48:02 +0100

    added messages model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :messages
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:3, maximum: 50}
     has_secure_password
 end
