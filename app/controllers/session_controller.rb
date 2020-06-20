f31a426ed09d2293bffe2d77ee33fddd42d82b15
themaleem <zizo9585@gmail.com>
2020-06-20 07:45:59 +0100

    task 2: add home and login routes

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,5 @@
+class SessionController < ApplicationController
+  def new; end
+  def create; end
+  def destroy; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get root 'pages#home' 
+  get 'login', to: 'session#new' 
+  get 'login', to: 'session#new' 
+  get 'logout', to: 'session#destroy' 
 end
